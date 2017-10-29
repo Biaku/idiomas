5e23d03d7c44c572db587206c6c4fbe4365a3946
HugoAlber0 <hugorivera.cool.player@gmail.com>
2017-10-28 20:04:50 -0500

    Añadida la grafica demostrativa de los grupos
    Se añadio una grafica de barras con Chart.JS

--- views/cpanel/admin/curso.php
+++ views/cpanel/admin/curso.php
@@ -1,5 +1,7 @@
 <?php include $templates_header ?>
     <body ng-app="admin" ng-controller="cursoController">
+    <script src="recursos\js\vendor\chartjs/Chart.bundle.js"></script>
+    <script src="recursos\js\vendor\chartjs\samples/utils.js"></script>
 <?php include $templates_navbar_adm ?>
     <br>
     <div class="container">
@@ -39,5 +41,79 @@
             </div>
         </div>
     </div>
+    <center><h2>Grafica de Barras de Alumnos por mes</h2>
+     <div id="container" style="width: 50%;">
+        <canvas id="canvas"></canvas>
+    </div>
+    <script>
+        var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var color = Chart.helpers.color;
+        var barChartData = {
+            labels: ["Enero","Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio","Agosto"],
+            datasets: [{
+                label: 'Alumnos por mes',
+                backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(),
+                borderColor: window.chartColors.blue,
+                borderWidth: 1,
+                data: [
+                    100,
+                    50,
+                    150,
+                    200,
+                    180,
+                    90,
+                    110,
+                    50
+                ]
+            }]
+
+        };
+
+        window.onload = function() {
+            var ctx = document.getElementById("canvas").getContext("2d");
+            window.myBar = new Chart(ctx, {
+                type: 'bar',
+                data: barChartData,
+                options: {
+                    responsive: true,
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Grafico Mensual de Inscripciones'
+                    }
+                }
+            });
+
+        };
+
+
+        var colorNames = Object.keys(window.chartColors);
+        document.getElementById('addDataset').addEventListener('click', function() {
+            var colorName = colorNames[barChartData.datasets.length % colorNames.length];;
+            var dsColor = window.chartColors[colorName];
+            var newDataset = {
+                label: 'Ventas ' + barChartData.datasets.length,
+                backgroundColor: color(dsColor).alpha(0.5).rgbString(),
+                borderColor: dsColor,
+                borderWidth: 1,
+                data: []
+            };
+
+            for (var index = 0; index < barChartData.labels.length; ++index) {
+                newDataset.data.push(randomScalingFactor());
+            }
+
+            barChartData.datasets.push(newDataset);
+            window.myBar.update();
+        });
+
+
+
+    </script>
+    </center>
+
+
 
 <?php include $templates_footer_adm ?>
