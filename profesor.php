c842a5ff98e2e7373925c52d3e1d1cd20cf9b7c7
Hugo Alberto Rivera Diaz <hugorivera.cool.player@gmail.com>
2017-11-22 21:08:20 -0600

    Clase de Conexion que Romero nos dio + PDF de la BD
    La clase class.connection.php es la clase que Romero nos dió y pues debe
    estar ahí. Ahora el PDF jala datos directamente de la base de datos.

--- profesor.php
+++ profesor.php
@@ -1,43 +1,19 @@
 <?php
-//============================================================+
-// File name   : example_011.php
-// Begin       : 2008-03-04
-// Last Update : 2013-05-14
-//
-// Description : Example 011 for TCPDF class
-//               Colored Table (very simple table)
-//
-// Author: Nicola Asuni
-//
-// (c) Copyright:
-//               Nicola Asuni
-//               Tecnick.com LTD
-//               www.tecnick.com
-//               info@tecnick.com
-//============================================================+
-
-/**
- * Creates an example PDF TEST document using TCPDF
- * @package com.tecnick.tcpdf
- * @abstract TCPDF - Example: Colored Table
- * @author Nicola Asuni
- * @since 2008-03-04
- */
-
 // Include the main TCPDF library (search for installation path).
 require_once('recursos\vendor\tcpdf\tcpdf.php');
+require("models\Conexion.php");
+require("models\class.connection.php");
+
 
 // extend TCPF with custom functions
-class MYPDF extends TCPDF {
+class MYPDF extends TCPDF
+ {
 
 	// Load table data from file
-	public function LoadData($file) {
-		// Read file lines
-		$lines = file($file);
-		$data = array();
-		foreach($lines as $line) {
-			$data[] = explode(';', chop($line));
-		}
+	public function LoadData() 
+	{
+		$db=Connection();
+	    $data = $db->recordSetArray("SELECT * FROM idioma");
 		return $data;
 	}
 
@@ -50,9 +26,10 @@ class MYPDF extends TCPDF {
 		$this->SetLineWidth(0.3);
 		$this->SetFont('', 'B');
 		// Header
-		$w = array(40, 35, 40, 45);
+		$w = array(35,45);
 		$num_headers = count($header);
-		for($i = 0; $i < $num_headers; ++$i) {
+		for($i = 0; $i < $num_headers; ++$i)
+		 {
 			$this->Cell($w[$i], 7, $header[$i], 1, 0, 'C', 1);
 		}
 		$this->Ln();
@@ -62,11 +39,10 @@ class MYPDF extends TCPDF {
 		$this->SetFont('');
 		// Data
 		$fill = 0;
-		foreach($data as $row) {
+		foreach($data as $row) 
+		{
 			$this->Cell($w[0], 6, $row[0], 'LR', 0, 'C', $fill);
-			$this->Cell($w[1], 6, $row[1], 'LR', 0, 'C', $fill);
-			$this->Cell($w[2], 6, $row[2], 'LR', 0, 'C', $fill);
-			$this->Cell($w[3], 6, $row[3], 'LR', 0, 'C', $fill);
+			$this->Cell($w[1], 6, $row[1], 'LR', 0, 'C', $fill);			
 			$this->Ln();
 			$fill=!$fill;
 		}
@@ -120,9 +96,8 @@ $pdf->SetFont('helvetica', '', 12);
 $pdf->AddPage();
 
 $txt = <<<EOD
-Listado de Grupos
-Profesor: Roberto Carlos Rivera Smith
-PROFESOR DE INGLES IMDI
+Listado de Idiomas
+Instituto de Idiomas de México
 
 EOD;
 
@@ -131,10 +106,10 @@ $pdf->Write(0, $txt, '', 0, 'C', true, 0, false, false, 0);
 
 
 // column titles
-$header = array('Hora', 'Idioma', 'Nivel', 'Aula');
+$header = array('Clave', 'Idioma');
 
 // data loading
-$data = $pdf->LoadData('data/table_data_demo.txt');
+$data = $pdf->LoadData();
 
 // print colored table
 $pdf->ColoredTable($header, $data);
